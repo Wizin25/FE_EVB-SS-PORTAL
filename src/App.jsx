711824ad1af3c35095d689838068e01231922265
Kamui <tranngoc5979@gmail.com>
2025-09-19 15:02:30 +0700

    Brand01

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './components/Log/signin';
 import SignUp from './components/Log/signup';
+import ForgotPassword from './components/Log/forgot';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<SignIn />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot" element={<ForgotPassword />} />
         </Routes>
       </div>
     </Router>
