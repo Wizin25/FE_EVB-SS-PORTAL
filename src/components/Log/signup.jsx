4d03421d4912d8b6a09de9efe795bd43e0eba27a
Kamui <tranngoc5979@gmail.com>
2025-09-19 17:17:39 +0700

    brand01

--- src/components/Log/signup.jsx
+++ src/components/Log/signup.jsx
@@ -38,6 +38,13 @@ function SignUp() {
         </div>
 
         <div className="sign-container">
+          <div style={{ position: 'absolute', top: -40, right: 0, padding: '0px' }}>
+          <img 
+            src="https://res.cloudinary.com/dzht29nkq/image/upload/v1758274139/SwapX_1_-Photoroom_wvmglm.png" 
+            alt="Brand Logo" 
+            style={{ maxWidth: '150px', height: 'auto', display: 'block' }}
+          />
+        </div>
           <h2>Sign Up</h2>
           <form onSubmit={handleSubmit}>
             <div className="input-group">
