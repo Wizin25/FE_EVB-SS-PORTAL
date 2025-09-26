e11164869b2eebcba130e2a30db81bbc8e13e29c
Kamui <tranngoc5979@gmail.com>
2025-09-26 17:00:19 +0700

    fixController

--- src/components/Admin/pages/Controller.jsx
+++ src/components/Admin/pages/Controller.jsx
@@ -6,5 +6,5 @@ export default function ControllerPage() {
       <h1 style={{ fontSize: 28, fontWeight: 700, }}>Controller User/Staff</h1>
       <p style={{ marginTop: 8 }}>Tổng quan hệ thống.</p>
     </div>
-  )
+  );
 }
